fda78fff8c27b24c4bc80f98a2fa7a7f32da2d43
a5-stable <sh07e1916@gmail.com>
2023-02-07 22:31:05 +0900

    categories

--- lib/feedly_api/client/categories.rb
+++ lib/feedly_api/client/categories.rb
@@ -3,16 +3,7 @@ module FeedlyApi
     module Categories
 
       def categories
-        options = { 
-          headers: {
-            Authorization: "Bearer #{access_token}"
-          }
-        }
-  
-        http_client = Faraday.new("https://cloud.feedly.com/v3/categories", options)
-        res = http_client.get
-  
-        require 'json'
+        res = get("categories")
         JSON.parse(res.body)
       end
     end
